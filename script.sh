c58393bdf78f19e1a331191937a4c3e59bdeb5b7
bobabebell <Bobabebell@gmail.com>
2022-01-10 14:22:24 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#Set the OUTPUT filename to 'backkup_' followed by date in form of %Y%m%d and flie's type is 'tgz' in home directory
 OUTPUT=~/backup_$(date +%Y%m%d).tgz
+
+#unarchive file OUTPUT to the directory argument 1
 tar -cvzf $OUTPUT $1
