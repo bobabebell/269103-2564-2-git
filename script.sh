d30ad7cddeca045e40c455adf7ccb4b4227bec4c
bobabebell <Bobabebell@gmail.com>
2022-01-10 14:26:10 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Set the OUTPUT filename to 'backkup_' followed by date in form of %Y%m%d and flie's type is 'tgz' in home directory
+#Set the OUTPUT filename to 'backup_' followed by date in form of %Y%m%d and flie's type is 'tgz' in home directory
 OUTPUT=~/backup_$(date +%Y%m%d).tgz
 
 #unarchive file OUTPUT to the directory argument 1
